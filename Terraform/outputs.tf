161449ef82252776be953cb510f497936b12a9af
Marco Solano <solanohma@gmail.com>
2026-07-17 07:11:42 -0600

    Changes on files

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,9 +1,9 @@
-output "EC2-Prometheus-IP" {
+output "Monitoring-Instance-Public-IP" {
   description = "EC2 instance public-IP for Prometheus instance: "
   value = module.ec2_instance.web1.EC2-instance-publicIP
 }
 
-output "EC2-Node-Exporter-IP" {
+output "Target-Instance-Public-IP" {
   description = "EC2 instance public-IP for Node-Exporter instance: "
   value = module.ec2_instance.web2.EC2-instance-publicIP
 }
